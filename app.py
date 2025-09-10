2c698e8fcf979c48e1d1f028b291441132ce5bcb
jSantosMv <jmurrugarra@ainnovatec.com>
2025-09-09 20:17:20 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API de prueba funcionando 🚀"
+
+@app.route("/procesar", methods=["POST"])
+def procesar():
+    data = request.get_json()
+    valor = data.get("valor", 0)
+    resultado = valor * 2
+    return jsonify({"resultado": resultado})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
